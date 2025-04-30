3c250b0ac5168f414ea02966cb8e1722b027791b
Taetae1123 <xodus2079@naver.com>
2025-04-30 20:04:43 +0900

    Fix: 상태 불러오기 요청 보내놓기

--- neul-front/src/features/StatusCheck/styled.ts
+++ neul-front/src/features/StatusCheck/styled.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const StatusCheckStyled = styled.div``;
+export const StatusCheckStyled = styled.div`
+  &.statuscheck_wrap {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 10px;
+    .statuscheck_name {
+      font-size: 22px;
+      font-weight: bolder;
+    }
+    .statuscheck_date {
+      float: right;
+    }
+  }
+`;
