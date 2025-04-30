341935f306e29431d2596d251c66b3f209fc429b
Taetae1123 <xodus2079@naver.com>
2025-04-30 16:15:24 +0900

    Fix: 채팅에 axiosinstance사용 및 변수 변경

--- neul-front/src/components/ChatMessage/index.tsx
+++ neul-front/src/components/ChatMessage/index.tsx
@@ -3,20 +3,20 @@ import { ChatMessageStyled } from "./styled";
 
 interface ChatMessageProps {
   name: string; // 도우미 이름
-  chatting: string; // 채팅 내용
+  message: string; // 채팅 내용
   time: string; // 보낸 시간
   isMe: boolean; // 본인이 작성한 chat인지 확인
 }
 
 // 채팅 메시지(본인이 작성한 것인지 판단)
-const ChatMessage = ({ name, chatting, time, isMe }: ChatMessageProps) => {
+const ChatMessage = ({ name, message, time, isMe }: ChatMessageProps) => {
   return (
     <ChatMessageStyled className={clsx("chatmessage_wrap")}>
       <div className={`${isMe ? "chatmessage_me" : "chatmessage_other"}`}>
         {!isMe && <div className="chatmessage_name">{name}</div>}
 
         <div className="chatmessage_chat">
-          <div className="chatmessage_text">{chatting}</div>
+          <div className="chatmessage_text">{message}</div>
           <div className="chatmessage_time">{time}</div>
         </div>
       </div>
