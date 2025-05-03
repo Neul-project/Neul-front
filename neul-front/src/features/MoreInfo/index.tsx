8e0cc28394b98ddf1370f34677f8c8bcb35cae15
werther <65219453+werther901@users.noreply.github.com>
2025-05-03 22:57:12 +0900

    Feat: Add MoreInfo

--- neul-front/src/features/MoreInfo/index.tsx
+++ neul-front/src/features/MoreInfo/index.tsx
@@ -4,6 +4,10 @@ const MoreInfo = () => {
   return (
     <MoreInfoStyled>
       <div>로그인 후 정보 더보기 컴포임</div>
+      <div>이름</div>
+      <div>전화번호</div>
+      <div>주소(선택)</div>
+      <div>피보호자 정보?</div>
     </MoreInfoStyled>
   );
 };
