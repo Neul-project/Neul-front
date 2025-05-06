7c8baf7950e34025bdc2fbc08d3a53c369f041f7
ihoek <ihoek679@naver.com>
2025-05-07 00:26:48 +0900

    fix : 음성 녹음 css 수정 및 요청 수정

--- neul-front/src/features/FeedBackModal/styled.ts
+++ neul-front/src/features/FeedBackModal/styled.ts
@@ -1,3 +1,79 @@
 import styled from "styled-components";
 
-export const FeedBackModalStyled = styled.div``;
+export const FeedBackModalStyled = styled.div`
+  &.FeedbackModal_main_wrap {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    gap: 10px;
+
+    .FeedbackModal_recording {
+      display: none;
+
+      .FeedbackModal_mic,
+      .FeedbackModal_pause {
+        cursor: pointer;
+      }
+    }
+    .FeedbackModal_controler {
+      display: none;
+    }
+    .FeedbackModal_footer {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+
+      .FeedbackModal_footer_send,
+      .FeedbackModal_footer_feeedback {
+        width: 120px;
+      }
+
+      .FeedbackModal_footer_send {
+        display: none;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    &.FeedbackModal_main_wrap {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      gap: 10px;
+
+      .FeedbackModal_textarea {
+        display: none;
+      }
+      .FeedbackModal_recording {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 10px;
+      }
+      .FeedbackModal_controler {
+        display: block;
+      }
+      .FeedbackModal_footer {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+
+        .FeedbackModal_footer_send,
+        .FeedbackModal_footer_feeedback {
+          width: 120px;
+        }
+
+        .FeedbackModal_footer_send {
+          display: flex;
+        }
+        .FeedbackModal_footer_feeedback {
+          display: none;
+        }
+      }
+    }
+  }
+`;
