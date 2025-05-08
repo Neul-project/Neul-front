dc4c59b29fefdf6dd8d76a16257551f737e3ea78
ihoek <ihoek679@naver.com>
2025-05-08 15:48:17 +0900

    fix : 광고 이미지 요청 수정

--- neul-front/src/features/Advertising/index.tsx
+++ neul-front/src/features/Advertising/index.tsx
@@ -4,21 +4,19 @@ import { AdvertisingStyled } from "./styled";
 //component
 import AdElement from "@/components/AdElement";
 
-//images
-import banner from "@/assets/images/banner.jpg";
 import { useEffect, useState } from "react";
 import axiosInstance from "@/lib/axios";
 
 //Advertising component
 const Advertising = () => {
-  const [arr, setArr] = useState([banner, banner]);
+  const [arr, setArr] = useState([]);
 
   //광고 이미지 요청
   useEffect(() => {
     axiosInstance.get("/banner/list").then((res) => {
-      console.log("banner Res", res.data);
-      const data = res.data[0].img.split(",");
-      //console.log(data.split(","));
+      //console.log("banner Res", res.data);
+      const datalist = res.data;
+      const data = res.data[datalist.length - 1].img.split(",");
       setArr(data);
     });
   }, []);
