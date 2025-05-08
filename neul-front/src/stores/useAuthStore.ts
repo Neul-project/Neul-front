31bb0deed1d69a3e8fe12eb16c76f5424db84972
werther <65219453+werther901@users.noreply.github.com>
2025-05-08 15:31:49 +0900

    Feat: 로그인 후 사용자 정보 요청

--- neul-front/src/stores/useAuthStore.ts
+++ neul-front/src/stores/useAuthStore.ts
@@ -1,5 +1,6 @@
 // stores/useAuthStore.ts
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type AuthState = {
   user: { id: number; name: string } | null;
@@ -8,9 +9,19 @@ type AuthState = {
   logout: () => void;
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  isLoggedIn: false,
-  login: (user) => set({ user, isLoggedIn: true }),
-  logout: () => set({ user: null, isLoggedIn: false }),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      isLoggedIn: false,
+      login: (user) => {
+        console.log("zustand login 실행됨, user:", user);
+        set({ user, isLoggedIn: true });
+      },
+      logout: () => set({ user: null, isLoggedIn: false }),
+    }),
+    {
+      name: "auth-storage", // localStorage에 저장될 키
+    }
+  )
+);
