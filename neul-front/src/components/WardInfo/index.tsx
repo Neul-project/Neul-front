962c79a07d3162d86c72731e9cc2c15c90fb5978
werther <65219453+werther901@users.noreply.github.com>
2025-05-08 12:41:05 +0900

    Feat: wardinfo ui

--- neul-front/src/components/WardInfo/index.tsx
+++ neul-front/src/components/WardInfo/index.tsx
@@ -3,7 +3,32 @@ import { WardInfoStyled } from "./styled";
 const WardInfo = () => {
   return (
     <WardInfoStyled>
-      <div>피보호자 정보 관리임</div>
+      <div className="MyInfo_container">
+        <div className="MyInfo_flex">
+          <div className="MyInfo_cont">
+            <div className="MyInfo_name">
+              <span>홍길동</span>님
+            </div>
+            <div className="MyInfo_email">abcd@abcd.com</div>
+          </div>
+
+          <div className="MyInfo_changePw">
+            <button type="button">비밀번호 변경</button>
+          </div>
+        </div>
+
+        <div className="MyInfo_phone">
+          <div className="title">휴대전화번호</div>
+          <div className="phone">010-1111-1111</div>
+        </div>
+
+        <div className="MyInfo_flex MyInfo_address">
+          <div>주소 관리</div>
+          <div className="MyInfo_changePw">
+            <button type="button">주소 등록</button>
+          </div>
+        </div>
+      </div>
     </WardInfoStyled>
   );
 };
