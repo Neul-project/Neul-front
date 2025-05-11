9ab0c9f6ce9a0829ec7c9a6ad3f728e87343f3e2
werther <65219453+werther901@users.noreply.github.com>
2025-05-11 20:31:44 +0900

    Fix: Edit mypage css

--- neul-front/src/features/MyPage/styled.ts
+++ neul-front/src/features/MyPage/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MyPageStyled = styled.div`
   max-width: 1280px;
   margin: 0 auto;
-  background-color: #edeff2; //f9fbfc
+  background-color: rgb(242, 245, 248);
 
   .MyPage_container {
     display: flex;
