d2557e99ca40a727e21c9852237f295cd03a913c
ihoek <ihoek679@naver.com>
2025-05-12 00:05:11 +0900

    fix : css 수정

--- neul-front/src/components/ProgramElement/styled.ts
+++ neul-front/src/components/ProgramElement/styled.ts
@@ -2,10 +2,32 @@ import styled from "styled-components";
 
 export const ProgramElementStyled = styled.div`
   &.ProgramElement_main_wrap {
-    width: 400px;
-    min-height: 300px;
-    border: 1px solid black;
+    width: 100%;
+    min-height: 200px;
     border-radius: 15px;
+    background: #5da487;
+    margin-bottom: 30px;
     cursor: pointer;
+
+    .ProgramElement_title {
+      padding: 5px;
+      font-size: 23px;
+      color: white;
+      font-weight: bold;
+    }
+    .ProgramElement_progress,
+    .ProgramElement_category,
+    .ProgramElement_recru,
+    .ProgramElement_capacity {
+      padding: 5px;
+      color: white;
+    }
+
+    .ProgramElement_img {
+      margin: 0px auto;
+      width: 90%;
+      height: 200px;
+      background-color: white;
+    }
   }
 `;
