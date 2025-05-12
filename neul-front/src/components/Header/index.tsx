a05dfe8c2d1cfdac87bf8a066c710f99d07eac4e
ihoek <ihoek679@naver.com>
2025-05-12 19:34:46 +0900

    fix : 활동기록 수정

--- neul-front/src/components/Header/index.tsx
+++ neul-front/src/components/Header/index.tsx
@@ -41,7 +41,7 @@ const Header = () => {
   };
 
   // 헤더 제외할 페이지
-  const notPage = ["/login", "/chat"];
+  const notPage = ["/login", "/chat", "/moreinfo"];
 
   const isHiddenStyle = notPage.includes(router.pathname);
 
