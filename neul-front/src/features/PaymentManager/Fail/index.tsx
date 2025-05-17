243ec4d58fc4ca950488e8c077f443f994702342
werther <65219453+werther901@users.noreply.github.com>
2025-05-17 19:32:24 +0900

    Feat: 결제 실패 페이지 css

--- neul-front/src/features/PaymentManager/Fail/index.tsx
+++ neul-front/src/features/PaymentManager/Fail/index.tsx
@@ -1,10 +1,30 @@
+import { useRouter } from "next/router";
 import { PaymentFailStyled } from "./styled";
 
 const PaymentFailFeat = () => {
+  const router = useRouter();
+
   return (
     <PaymentFailStyled>
-      <div>결제 실패 페이지임</div>
-      <div>결제 중 오류가 발생했습니다.</div>
+      <div className="Fail_container">
+        <div className="Fail_X">
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+
+        <div className="Fail_title">결제 실패</div>
+        <div>결제 중 오류가 발생했습니다.</div>
+        <div>결제를 다시 시도하세요.</div>
+
+        <div className="Fail_btn">
+          <button
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            홈으로 이동
+          </button>
+        </div>
+      </div>
     </PaymentFailStyled>
   );
 };
--- neul-front/src/features/PaymentManager/Fail/styled.ts
+++ neul-front/src/features/PaymentManager/Fail/styled.ts
@@ -2,4 +2,51 @@ import styled from "styled-components";
 
 export const PaymentFailStyled = styled.div`
   background-color: ${(props) => props.theme.colors.backColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+  padding: 20px;
+
+  .Fail_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 450px;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 16px;
+    margin-bottom: 15px;
+
+    @media (max-width: 632px) {
+      width: 100%;
+    }
+  }
+
+  .Fail_X {
+    font-size: 40px;
+    color: #e50000;
+  }
+
+  .Fail_title {
+    font-size: 24px;
+    font-weight: 700;
+    margin-bottom: 20px;
+    color: #333;
+  }
+
+  .Fail_btn {
+    margin-top: 20px;
+
+    button {
+      cursor: pointer;
+      width: 100%;
+      padding: 9px 18px;
+      background-color: #fff;
+      color: #8cc2a9;
+      border: 1px solid #8cc2a9;
+      border-radius: 20px;
+      font-size: 14px;
+    }
+  }
 `;
