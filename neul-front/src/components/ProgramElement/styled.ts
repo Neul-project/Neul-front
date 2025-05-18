c2c32f7f1a73bf44b43931f20370908da0dfcadc
ihoek <ihoek679@naver.com>
2025-05-18 17:26:47 +0900

    fix : 프로그램 리스트 반응형

--- neul-front/src/components/ProgramElement/styled.ts
+++ neul-front/src/components/ProgramElement/styled.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const ProgramElementStyled = styled.div`
   &.ProgramElement_main_wrap {
-    flex: 0 0 25%;
-    padding: 0 15px;
+    //padding: 0 15px;
     cursor: pointer;
+    width: 100%;
+    box-sizing: border-box;
 
     .ProgramElement_img {
       width: 100%;
       height: 280px;
-      //object-fit: cover;
       background: #e5e5e5;
       border-radius: 5px;
       overflow: hidden;
@@ -17,6 +17,8 @@ export const ProgramElementStyled = styled.div`
       .ProgramElement_imgstyle {
         display: block;
         width: 100%;
+        height: 100%;
+        object-fit: cover;
         transition: transform 0.3s;
       }
 
@@ -38,6 +40,7 @@ export const ProgramElementStyled = styled.div`
         background: #e9f5f3;
         border-radius: 3px;
       }
+
       /* 모집완료일 때 배경색 */
       .ProgramElement_Recruit_end {
         background-color: #f0f0f0;
@@ -45,12 +48,13 @@ export const ProgramElementStyled = styled.div`
       }
       .ProgramElement_title {
         width: 100%;
-        max-width: 280px;
+        max-width: 160px;
+        //max-width: 230px;
+        //background-color: red;
         margin-top: 10px;
-        font-family: "GmarketSans";
         font-weight: 500;
-        font-size: 25px;
-
+        font-size: 22px;
+        width: 100%;
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
@@ -66,6 +70,229 @@ export const ProgramElementStyled = styled.div`
         .ProgramElement_progress,
         .ProgramElement_recru {
           padding-bottom: 3px;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    &.ProgramElement_main_wrap {
+      //padding: 0 15px;
+      cursor: pointer;
+      width: 100%;
+      box-sizing: border-box;
+
+      .ProgramElement_img {
+        width: 100%;
+        height: 250px;
+        //object-fit: cover;
+        background: #e5e5e5;
+        border-radius: 5px;
+        overflow: hidden;
+
+        .ProgramElement_imgstyle {
+          display: block;
+          width: 100%;
+          transition: transform 0.3s;
+        }
+
+        &:hover .ProgramElement_imgstyle {
+          transform: scale(1.05); /* 5% 확대 */
+        }
+      }
+
+      .ProgramElement_content {
+        padding: 10px 0;
+        text-align: left;
+
+        .ProgramElement_Recruit {
+          display: inline-block;
+          padding: 5px 6px;
+          font-weight: 600;
+          font-size: 11px;
+          color: #13453c;
+          background: #e9f5f3;
+          border-radius: 3px;
+        }
+
+        /* 모집완료일 때 배경색 */
+        .ProgramElement_Recruit_end {
+          background-color: #f0f0f0;
+          color: #999;
+        }
+        .ProgramElement_title {
+          width: 100%;
+          max-width: 160px;
+          //max-width: 230px;
+          //background-color: red;
+          margin-top: 10px;
+          font-weight: 500;
+          font-size: 18px;
+          width: 100%;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+
+        .ProgramElement_botton_content {
+          margin-top: 10px;
+          padding-top: 10px;
+          border-top: 1px dashed #ccc;
+          font-size: 13px;
+
+          .ProgramElement_category,
+          .ProgramElement_progress,
+          .ProgramElement_recru {
+            padding-bottom: 3px;
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 486px) {
+    &.ProgramElement_main_wrap {
+      //padding: 0 15px;
+      cursor: pointer;
+      width: 100%;
+      box-sizing: border-box;
+
+      .ProgramElement_img {
+        width: 100%;
+        height: 230px;
+        background: #e5e5e5;
+        border-radius: 5px;
+        overflow: hidden;
+
+        .ProgramElement_imgstyle {
+          display: block;
+          width: 100%;
+          transition: transform 0.3s;
+        }
+
+        &:hover .ProgramElement_imgstyle {
+          transform: scale(1.05); /* 5% 확대 */
+        }
+      }
+
+      .ProgramElement_content {
+        padding: 8px 0;
+        text-align: left;
+
+        .ProgramElement_Recruit {
+          display: inline-block;
+          padding: 5px 6px;
+          font-weight: 600;
+          font-size: 11px;
+          color: #13453c;
+          background: #e9f5f3;
+          border-radius: 3px;
+        }
+
+        /* 모집완료일 때 배경색 */
+        .ProgramElement_Recruit_end {
+          background-color: #f0f0f0;
+          color: #999;
+        }
+        .ProgramElement_title {
+          width: 100%;
+          max-width: 160px;
+          //max-width: 230px;
+          //background-color: red;
+          margin-top: 10px;
+          font-weight: 500;
+          font-size: 18px;
+          width: 100%;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+
+        .ProgramElement_botton_content {
+          margin-top: 10px;
+          padding-top: 10px;
+          border-top: 1px dashed #ccc;
+          font-size: 11px;
+
+          .ProgramElement_category,
+          .ProgramElement_progress,
+          .ProgramElement_recru {
+            padding-bottom: 3px;
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 375px) {
+    &.ProgramElement_main_wrap {
+      //padding: 0 15px;
+      cursor: pointer;
+      width: 100%;
+      box-sizing: border-box;
+
+      .ProgramElement_img {
+        width: 100%;
+        height: 230px;
+        background: #e5e5e5;
+        border-radius: 5px;
+        overflow: hidden;
+
+        .ProgramElement_imgstyle {
+          display: block;
+          width: 100%;
+          transition: transform 0.3s;
+        }
+
+        &:hover .ProgramElement_imgstyle {
+          transform: scale(1.05); /* 5% 확대 */
+        }
+      }
+
+      .ProgramElement_content {
+        padding: 8px 0;
+        text-align: left;
+
+        .ProgramElement_Recruit {
+          display: inline-block;
+          padding: 5px 6px;
+          font-weight: 600;
+          font-size: 11px;
+          color: #13453c;
+          background: #e9f5f3;
+          border-radius: 3px;
+        }
+
+        /* 모집완료일 때 배경색 */
+        .ProgramElement_Recruit_end {
+          background-color: #f0f0f0;
+          color: #999;
+        }
+        .ProgramElement_title {
+          width: 100%;
+          max-width: 200px;
+          //max-width: 230px;
+          //background-color: red;
+          margin-top: 10px;
+          font-weight: 500;
+          font-size: 18px;
+          width: 100%;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+
+        .ProgramElement_botton_content {
+          margin-top: 10px;
+          padding-top: 10px;
+          border-top: 1px dashed #ccc;
+          font-size: 11px;
+
+          .ProgramElement_category,
+          .ProgramElement_progress,
+          .ProgramElement_recru {
+            padding-bottom: 3px;
+          }
         }
       }
     }
--- neul-front/src/features/ProgramList/styled.ts
+++ neul-front/src/features/ProgramList/styled.ts
@@ -19,26 +19,140 @@ export const ProgramListStyled = styled.div`
       //margin-bottom: 30px;
       padding: 20px 0;
       align-items: center;
-      font-size: 25px;
+      font-size: 30px;
       //margin-bottom: 60px;
       font-weight: bolder;
     }
     .ProgramList_subtitle {
-      border-top: 1px solid #ccc;
+      //border-top: 1px solid #ccc;
       display: flex;
       flex-direction: row;
       align-items: center;
-
-      padding: 10px 0px;
+      justify-content: end;
+      //padding: 10px 0px;
     }
 
     .ProgramList_grid_wrap {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr); /* 4열로 고정 */
+      gap: 20px;
+      padding: 20px 0;
+      //border-bottom: 1px solid #ccc;
+      //border-top: 1px solid #ccc;
+    }
+
+    .ProgramList_pagination {
+      /* 페이지 번호 버튼 테두리 */
+      .ant-pagination-item {
+        border-color: none !important; /* 원하는 테두리 색상으로 변경 */
+      }
+      /* 활성화된 페이지 버튼 테두리 */
+      .ant-pagination-item-active {
+        border-color: #5da487 !important;
+      }
+
+      .ant-pagination-item-active a {
+        color: #5da487;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    &.ProgramList_main_wrap {
+      max-width: 1280px;
+      width: 100%;
       display: flex;
-      flex-wrap: wrap;
-      gap: 30px 0px;
-      padding: 30px 0;
-      border-bottom: 1px solid #ccc;
-      border-top: 1px solid #ccc;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      margin: 0px auto;
+      padding: 0px 20px;
+
+      .ProgramList_title {
+        display: flex;
+        //align-items: flex-end;
+        align-items: center;
+        justify-content: space-between;
+        //margin-bottom: 30px;
+        padding: 20px 0;
+        align-items: center;
+        font-size: 25px;
+        //margin-bottom: 60px;
+        font-weight: bolder;
+      }
+      .ProgramList_subtitle {
+        //border-top: 1px solid #ccc;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: end;
+        //padding: 10px 0px;
+      }
+
+      .ProgramList_grid_wrap {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr); /* 4열로 고정 */
+        gap: 20px;
+        padding: 20px 0;
+        //border-bottom: 1px solid #ccc;
+        //border-top: 1px solid #ccc;
+      }
+    }
+  }
+
+  @media (max-width: 486px) {
+    &.ProgramList_main_wrap {
+      max-width: 1280px;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      margin: 0px auto;
+      padding: 0px 20px;
+
+      .ProgramList_title {
+        display: flex;
+        //align-items: flex-end;
+        align-items: center;
+        justify-content: space-between;
+        //margin-bottom: 30px;
+        padding: 20px 0;
+        align-items: center;
+        font-size: 20px;
+        //margin-bottom: 60px;
+        font-weight: bolder;
+      }
+      .ProgramList_subtitle {
+        //border-top: 1px solid #ccc;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: end;
+        //padding: 10px 0px;
+      }
+
+      .ProgramList_grid_wrap {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 20px;
+        padding: 20px 0;
+        //border-bottom: 1px solid #ccc;
+        //border-top: 1px solid #ccc;
+      }
+    }
+  }
+
+  @media (max-width: 375px) {
+    &.ProgramList_main_wrap {
+      .ProgramList_grid_wrap {
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        gap: 20px;
+        padding: 20px 0;
+        //border-bottom: 1px solid #ccc;
+        //border-top: 1px solid #ccc;
+      }
     }
   }
 `;
