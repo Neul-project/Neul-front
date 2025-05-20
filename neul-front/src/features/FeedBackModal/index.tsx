b61f71cacb73e6df7584c1a4985f097b318f83ac
ihoek <ihoek679@naver.com>
2025-05-21 00:32:18 +0900

    fix : 토스 에디터 view 추가(css X) 라우터 오류 수정

--- neul-front/src/features/FeedBackModal/index.tsx
+++ neul-front/src/features/FeedBackModal/index.tsx
@@ -13,29 +13,10 @@ import { AntdGlobalTheme } from "@/utils/antdtheme";
 const FeedBackModal = (props: { activityid: string; onClose: () => void }) => {
   //변수 선언
   const { activityid, onClose } = props;
-  const [selectedType, setSelectedType] = useState<"text" | "audio">("text");
 
   return (
     <FeedBackModalStyled className={clsx("FeedbackModal_main_wrap")}>
-      <div className="FeedbackModal_radio_group">
-        <ConfigProvider theme={AntdGlobalTheme}>
-          <Radio.Group
-            onChange={(e) => setSelectedType(e.target.value)}
-            value={selectedType}
-            buttonStyle="solid"
-          >
-            <Radio value="text">텍스트</Radio>
-            <Radio value="audio">오디오</Radio>
-          </Radio.Group>
-        </ConfigProvider>
-      </div>
-
-      {/* 조건부 렌더링 */}
-      {selectedType === "audio" ? (
-        <FeedBackAudio activityid={activityid} onClose={onClose} />
-      ) : (
-        <FeedBackText activityid={activityid} onClose={onClose} />
-      )}
+      <FeedBackText activityid={activityid} onClose={onClose} />
     </FeedBackModalStyled>
   );
 };
