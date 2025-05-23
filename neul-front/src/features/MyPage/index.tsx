a80f9a34342c5faeb1aade3458a91dd9e7607e40
Taetae1123 <xodus2079@naver.com>
2025-05-23 17:41:38 +0900

    Fix: 마이페이지 최적화(쿼리 빼고)

--- neul-front/src/features/MyPage/index.tsx
+++ neul-front/src/features/MyPage/index.tsx
@@ -1,4 +1,3 @@
-import { useRouter, useSearchParams } from "next/navigation";
 import { MyPageStyled } from "./styled";
 
 import SidebarMenu from "@/components/SideBarMenu";
@@ -9,44 +8,50 @@ import HelperApplication from "@/components/HelperApplication";
 import ApprovalHistory from "@/components/ApprovalHistory";
 
 import { useAuthStore } from "@/stores/useAuthStore";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-export type TabType =
-  | "myinfo"
-  | "wardinfo"
-  | "program"
-  | "helper_application"
-  | "approval_history";
+// export type TabType =
+//   | "myinfo"
+//   | "wardinfo"
+//   | "program"
+//   | "helper_application"
+//   | "approval_history";
 
 // 마이페이지 메인 컴포넌트
 // 왼쪽 사이드바에서 메뉴 선택 시 쿼리스트링을 통해 URL 상태 반영
 const MyPageCompo = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
+
+  const [type, setType] = useState("myinfo");
+
+  // const searchParams = useSearchParams();
   // 현재 URL의 tab 값을 읽어 활성화된 메뉴 판단
-  const currentTab = (searchParams.get("tab") as TabType) || "myinfo";
+  // const currentTab = (searchParams.get("tab") as TabType) || "myinfo";
 
   // 탭을 클릭할 때 URL 쿼리스트링 변경
-  const setTab = (tab: TabType) => {
-    router.push(`?tab=${tab}`);
-  };
+  // const setTab = (tab: TabType) => {
+  //   router.push(`?tab=${tab}`);
+
+  // };
 
   // 메뉴에 따른 콘텐츠 컴포넌트 조건 렌더링
-  const renderContent = () => {
-    switch (currentTab) {
-      case "myinfo":
-        return <MyInfo />;
-      case "wardinfo":
-        return <WardInfo />;
-      case "program":
-        return <ProgramHistory />;
-      case "helper_application":
-        return <HelperApplication />;
-      case "approval_history":
-        return <ApprovalHistory />;
-      default:
-        return <div>탭을 찾을 수 없습니다.</div>;
-    }
-  };
+  // const renderContent = () => {
+  //   switch (currentTab) {
+  //     case "myinfo":
+  //       return <MyInfo />;
+  //     case "wardinfo":
+  //       return <WardInfo />;
+  //     case "program":
+  //       return <ProgramHistory />;
+  //     case "helper_application":
+  //       return <HelperApplication />;
+  //     case "approval_history":
+  //       return <ApprovalHistory />;
+  //     default:
+  //       return <div>탭을 찾을 수 없습니다.</div>;
+  //   }
+  // };
 
   return (
     <MyPageStyled>
@@ -54,8 +59,8 @@ const MyPageCompo = () => {
         {/* 왼쪽 메뉴바 */}
         <div className="MyPage_leftContainer">
           <SidebarMenu
-            currentTab={currentTab}
-            onTabChange={setTab}
+            currentTab={type}
+            onTabChange={setType}
             onLogout={() => {
               // 상태 초기화 + 쿠키 삭제
               useAuthStore.getState().logout();
@@ -65,7 +70,14 @@ const MyPageCompo = () => {
         </div>
 
         {/* 오른쪽 컨텐츠 내용 */}
-        <div className="MyPage_rightContainer">{renderContent()}</div>
+        <div className="MyPage_rightContainer">
+          {/* {renderContent()} */}
+          {type === "myinfo" && <MyInfo />}
+          {type === "wardinfo" && <WardInfo />}
+          {type === "program" && <ProgramHistory />}
+          {type === "helper_application" && <HelperApplication />}
+          {type === "approval_history" && <ApprovalHistory />}
+        </div>
       </div>
     </MyPageStyled>
   );
